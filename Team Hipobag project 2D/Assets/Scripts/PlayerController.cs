1d3e6408b8582b85018f32ae314f79e0d76594ab
CJMoneyBags <49886936+CJMoneyBags@users.noreply.github.com>
2022-10-29 23:12:31 -0600

    Fixed jumping issues and added an ability to double jump

--- Team Hipobag project 2D/Assets/Scripts/PlayerController.cs
+++ Team Hipobag project 2D/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,11 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float jumpForce;
 
-    //private bool isGrounded;
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    private bool canDoubleJump;
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +25,27 @@ public class PlayerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
 
-        //isGrounded = Physics2D.OverlapCircle()
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
 
         if (Input.GetButtonDown("Jump")) 
         {
-            //if(isGrounded)
-            //{
+            if(isGrounded)
+            {
                 theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-            //}
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
         }
     }
 }
