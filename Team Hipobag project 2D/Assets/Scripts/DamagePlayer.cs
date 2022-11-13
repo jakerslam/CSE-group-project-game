bc1fafa38b301446e0c78c2e2220d57a4b8c9cff
CJMoneyBags <49886936+CJMoneyBags@users.noreply.github.com>
2022-11-12 22:43:25 -0700

    Update

--- Team Hipobag project 2D/Assets/Scripts/DamagePlayer.cs
+++ Team Hipobag project 2D/Assets/Scripts/DamagePlayer.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class DamagePlayer : MonoBehaviour
 {
-    public int test;
+    //public float countDown = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (EditorSceneManager.GetActiveScene().name == "Testing2")
+        //{
+        //    countDown -= Time.deltaTime;
+        //    if (countDown <= 0)
+        //    {
+        //        EditorSceneManager.LoadScene(sceneName: "Testing");
+        //        countDown = 10;
+
+        //    }
+
+        //}
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerHealthController.instance.DealDamage();
+
+            //EditorSceneManager.LoadScene(sceneName: "Testing2");
+
+            
+        }
         
     }
 }
---
+++ Team Hipobag project 2D/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth, maxHealth;
+
+    public float invincibleLength;
+    private float invincibleCount;
+
+    private SpriteRenderer theSR;
+
+    private void Awake()
+    {
+        instance = this; //can access from other scripts
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(invincibleCount > 0)
+        {
+            invincibleCount -= Time.deltaTime;
+            
+            if(invincibleCount <= 0)
+            {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+
+            }
+        }
+    }
+
+    public void DealDamage()
+    {
+        if (invincibleCount <= 0)
+        { 
+            currentHealth--;
+
+            if(currentHealth <= 0)
+            {
+            currentHealth = 0;
+            gameObject.SetActive(false);
+            }
+            else
+            {
+            invincibleCount = invincibleLength;
+
+            theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b,  0.5f);
+
+            }
+        }
+    }
+}
